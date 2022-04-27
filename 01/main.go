b7ff9ab98cdf7b36217a846061e4d18043de7ad7
Administrator <admin@example.com>
2022-04-27 23:27:47 +0800

    init add exp inherit

--- 01/main.go
+++ 01/main.go
@@ -1,9 +1,38 @@
 package main
 
+import "sync"
+
+/*
+Type Declarations and Methods
+*/
+
+//error
+type myMutex sync.Mutex
+
+
+//inherit from base struct: embedding the original type as an anonymous field
+type myLocker struct {
+	sync.Mutex
+}
+
 
 
 
 func main() {
+
+	var mtx sync.Mutex
+	mtx.Lock()
+	mtx.Unlock()
+
+/*
+	var mtx2 myMutex
+	myMutex.Lock()
+	myMutex.Unlock()
+*/
+
+	var lock myLocker
+	lock.Lock()
+	lock.Unlock()
 
 
 
